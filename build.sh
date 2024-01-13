ff021a00da55706c519e16ebf5ae65d9b5b959b5
Omer Šabić <omer@datalinx.si>
2024-01-13 13:27:53 +0100

    feat: add tree, nano and nodejs

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 echo "Building Docker images..."
 
-docker build -t "slimdeluxe/php:7.4" -f "php-7.4/Dockerfile" .
-docker build -t "slimdeluxe/php:8.1" -f "php-8.1/Dockerfile" .
-docker build -t "slimdeluxe/php:8.2" -f "php-8.2/Dockerfile" .
+docker build -t "slimdeluxe/php:7.4-v1.0" -f "php-7.4/Dockerfile" .
+docker build -t "slimdeluxe/php:8.1-v1.0" -f "php-8.1/Dockerfile" .
+docker build -t "slimdeluxe/php:8.2-v1.0" -f "php-8.2/Dockerfile" .
 
 echo "Build done!"
 
--- push.sh
+++ push.sh
@@ -2,9 +2,9 @@
 
 echo "Pushing images to Docker Hub..."
 
-docker push slimdeluxe/php:7.4
-docker push slimdeluxe/php:8.1
-docker push slimdeluxe/php:8.2
+docker push slimdeluxe/php:7.4-v1.0
+docker push slimdeluxe/php:8.1-v1.0
+docker push slimdeluxe/php:8.2-v1.0
 
 echo "Push done!"
 
