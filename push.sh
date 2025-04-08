ae2af952f776b01f4570e74de97b6702149ac669
Omer Šabić <omer@datalinx.si>
2025-04-08 08:38:24 +0200

    feat: add PHP 8.3

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@ echo "Pushing images to Docker Hub..."
 docker push slimdeluxe/php:7.4-v1.0
 docker push slimdeluxe/php:8.1-v1.0
 docker push slimdeluxe/php:8.2-v1.0
+docker push slimdeluxe/php:8.3-v1.0
 
 echo "Push done!"
 
