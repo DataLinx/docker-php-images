e7d42e12618e4b2a3b25221c96e223e635b6ffd1
Omer Šabić <omer@datalinx.si>
2025-04-09 11:59:28 +0200

    feat: add screen and zsh

--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 
 echo "Pushing images to Docker Hub..."
 
-docker push slimdeluxe/php:7.4-v1.0
-docker push slimdeluxe/php:8.1-v1.0
-docker push slimdeluxe/php:8.2-v1.0
-docker push slimdeluxe/php:8.3-v1.0
+docker push slimdeluxe/php:7.4-v1.1
+docker push slimdeluxe/php:8.1-v1.1
+docker push slimdeluxe/php:8.2-v1.1
+docker push slimdeluxe/php:8.3-v1.1
 
 echo "Push done!"
 
