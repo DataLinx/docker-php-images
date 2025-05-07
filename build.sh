599d9ffc9ab5e7233be1463f40b6bb7b8b58bb7d
Omer Šabić <omer@datalinx.si>
2025-05-07 10:01:22 +0200

    Update Dockerfiles and build scripts for improved functionality
    Added `supervisor`, `sudo`, and `cron` to system tools in all PHP Dockerfiles. Allowed `www-data` user to execute sudo commands without a password. Updated image tags in the build and push scripts to reflect version `v1.2`.

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 echo "Building Docker images..."
 
-docker build -t "slimdeluxe/php:7.4-v1.1" -f "php-7.4/Dockerfile" .
-docker build -t "slimdeluxe/php:8.1-v1.1" -f "php-8.1/Dockerfile" .
-docker build -t "slimdeluxe/php:8.2-v1.1" -f "php-8.2/Dockerfile" .
-docker build -t "slimdeluxe/php:8.3-v1.1" -f "php-8.3/Dockerfile" .
+docker build -t "slimdeluxe/php:7.4-v1.2" -f "php-7.4/Dockerfile" .
+docker build -t "slimdeluxe/php:8.1-v1.2" -f "php-8.1/Dockerfile" .
+docker build -t "slimdeluxe/php:8.2-v1.2" -f "php-8.2/Dockerfile" .
+docker build -t "slimdeluxe/php:8.3-v1.2" -f "php-8.3/Dockerfile" .
 
 echo "Build done!"
 
--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 
 echo "Pushing images to Docker Hub..."
 
-docker push slimdeluxe/php:7.4-v1.1
-docker push slimdeluxe/php:8.1-v1.1
-docker push slimdeluxe/php:8.2-v1.1
-docker push slimdeluxe/php:8.3-v1.1
+docker push slimdeluxe/php:7.4-v1.2
+docker push slimdeluxe/php:8.1-v1.2
+docker push slimdeluxe/php:8.2-v1.2
+docker push slimdeluxe/php:8.3-v1.2
 
 echo "Push done!"
 
